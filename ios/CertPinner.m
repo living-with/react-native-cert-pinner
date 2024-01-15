1c13a3d8a395d1508fc54eed577ee93bcec329c1
Yupeng-li <31921080+Yupeng-li@users.noreply.github.com>
2024-01-15 10:22:03 +0000

    Update CertPinner.m to no longer use code which isn't available when building Pod as a framework

--- ios/CertPinner.m
+++ ios/CertPinner.m
@@ -1,7 +1,5 @@
 
 #import "CertPinner.h"
-#import <TrustKit/TSKSPKIHashCache.h>
-#import <TrustKit/reporting_utils.h>
 #import <Sentry/SentrySDK.h>
 
 @import TrustKit;
@@ -23,12 +21,8 @@ static TSKSPKIHashCache *spkiHashCache;
 - (instancetype)init {
     self = [super init];
 
-    spkiHashCache = [[TSKSPKIHashCache alloc] initWithIdentifier:@"cert-pinner.spki-hash.cache"];
-
     [[TrustKit sharedInstance] setPinningValidatorCallback:^(TSKPinningValidatorResult *_Nonnull result, NSString *_Nonnull notedHostname, TKSDomainPinningPolicy *_Nonnull policy) {
         if (result.evaluationResult != TSKTrustEvaluationSuccess) {
-            NSArray *formattedPins = convertPinsToHpkpPins(policy[kTSKPublicKeyHashes]);
-
             NSMutableArray *peerSpkiHashes = [[NSMutableArray alloc] initWithCapacity:result.certificateChain.count];
             NSMutableArray *peerCommonNames = [[NSMutableArray alloc] initWithCapacity:result.certificateChain.count];
             for (id certificateString in result.certificateChain) {
@@ -39,21 +33,13 @@ static TSKSPKIHashCache *spkiHashCache;
 
                 NSData *der = [[NSData alloc] initWithBase64EncodedString:pemString options:NSDataBase64DecodingIgnoreUnknownCharacters];
                 SecCertificateRef certificate = SecCertificateCreateWithData(kCFAllocatorDefault, (__bridge CFDataRef) der);
-                NSData *subjectPublicKeyInfoHash = [spkiHashCache hashSubjectPublicKeyInfoFromCertificate:certificate];
-                [peerSpkiHashes addObject:subjectPublicKeyInfoHash];
                 [peerCommonNames addObject:(NSString *) CFBridgingRelease(SecCertificateCopySubjectSummary(certificate))];
-            }
-            NSArray *formattedPeers = convertPinsToHpkpPins([peerSpkiHashes copy]);
-            NSMutableArray *formattedPeersWithCommonNames = [[NSMutableArray alloc] initWithCapacity:formattedPeers.count];
-            for (int i = 0; i < formattedPeers.count; i++) {
-                [formattedPeersWithCommonNames addObject:[NSString stringWithFormat:@"%@: CN=%@", formattedPeers[i], peerCommonNames[i]]];
             }
 
             NSDictionary *userInfo = @{
-                    @"Peer certificate chain": formattedPeersWithCommonNames,
                     @"Pinned domain": notedHostname,
-                    @"Pinned certificates": formattedPins,
-                    @"Peer certificate PEMs:": result.certificateChain
+                    @"Peer certificate PEMs:": result.certificateChain,                    
+                    @"Peer certificate common names:": peerCommonNames
             };
             NSError *error = [NSError errorWithDomain:@"PinningValidationError" code:-1001 userInfo:userInfo];
             [SentrySDK captureError:error];
